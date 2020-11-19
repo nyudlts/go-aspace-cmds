9830ce37b41dee930bb61f2041c3d82d0fff97b9
Don Mennerich <don.mennerich@nyu.edu>
2020-11-19 15:14:50 -0500

    Adding resource validation

--- cmd/validate.go
+++ cmd/validate.go
@@ -9,22 +9,33 @@ import (
 	"path/filepath"
 )
 
+var validationType string
+
 var validateCmd = &cobra.Command{
-	Use: "validate",
+	Use:   "validate",
 	Short: "Validate ead exports for a repository",
 	Run: func(cmd *cobra.Command, args []string) {
 		client, err := aspace.NewClient(env, timeout)
 		if err != nil {
 			HandleError(err)
 		}
-		validateRepository(client)
+		if validationType == "repository" {
+			validateRepository(client)
+		} else if validationType == "resource" {
+			validateResource(client)
+		} else {
+			panic(fmt.Errorf("type argument must be either 'repository' or 'resource'"))
+		}
+
 	},
 }
 
 func init() {
 	validateCmd.Flags().StringVarP(&env, "environment", "e", "dev", "ArchivesSpace environment to be used for export")
-	validateCmd.Flags().IntVar(&repositoryId, "repository", 2, "Repository to be used for export")
+	validateCmd.Flags().IntVar(&repositoryId, "repository", 2, "Repository Id to be used for validated")
+	validateCmd.Flags().IntVar(&resourceId, "resource", 1, "Resource Id to be validated")
 	validateCmd.Flags().IntVar(&timeout, "timeout", 20, "server timeout")
+	validateCmd.Flags().StringVarP(&validationType, "type", "t", "", "type of validation to perform")
 	rootCmd.AddCommand(validateCmd)
 }
 
@@ -38,16 +49,17 @@ func validateRepository(client *aspace.ASClient) {
 	resourceIds, err := client.GetResourceIDs(repositoryId)
 	HandleError(err)
 
-	for i, resourceId := range resourceIds {
-		resource, err := client.GetResource(repositoryId, resourceId)
+	for i, resId := range resourceIds {
+		resource, err := client.GetResource(repositoryId, resId)
 		HandleError(err)
 		fmt.Print(i, " ", resource.Title)
-		ead, err := client.SerializeEAD(repositoryId, resourceId, true, false, false, false, false)
-		err = aspace.ValidateEAD(ead); if err != nil {
-			fmt.Print( "\t *Failed Validation*\n")
-			writer.WriteString(resource.URI +"\n")
+		ead, err := client.SerializeEAD(repositoryId, resId, true, false, false, false, false)
+		err = aspace.ValidateEAD(ead)
+		if err != nil {
+			fmt.Print("\t *Failed Validation*\n")
+			writer.WriteString(resource.URI + "\n")
 			writer.Flush()
-			outEAD, innerErr := os.Create(filepath.Join("failures", resource.EADID + ".xml"))
+			outEAD, innerErr := os.Create(filepath.Join("failures", resource.EADID+".xml"))
 			HandleError(innerErr)
 			eadWriter := bufio.NewWriter(outEAD)
 			eadWriter.Write(ead)
@@ -56,5 +68,17 @@ func validateRepository(client *aspace.ASClient) {
 			fmt.Print("\t *Passed Validation*\n")
 		}
 	}
+}
 
+func validateResource(client *aspace.ASClient) {
+	resource, err := client.GetResource(repositoryId, resourceId)
+	HandleError(err)
+	ead, err := client.SerializeEAD(repositoryId, resourceId, true, false, false, false, false)
+	HandleError(err)
+	err = aspace.ValidateEAD(ead)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resource.Title, " passed validation")
+	}
 }
