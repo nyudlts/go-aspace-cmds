7ba48b87b110183a163b21ba5ca3ac95de7880ff
Don Mennerich <don.mennerich@nyu.edu>
2021-01-11 11:47:16 -0500

    moving all to export-repo

--- cmd/export-repo.go
+++ cmd/export-repo.go
@@ -2,18 +2,17 @@ package cmd
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"github.com/nyudlts/go-aspace"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
-	"time"
 )
 
-var repoID int
-
 func init() {
-	exportRepoCmd.Flags().IntVarP(&repoID, "repository", "r", 2,"The Id of the Repository to be Exported")
+	exportRepoCmd.Flags().StringVarP(&env, "environment", "e", "dev", "ArchivesSpace environment to be used for export")
+	exportRepoCmd.Flags().IntVarP(&repositoryId, "repository", "r", 2, "Repository to be used for export")
 	rootCmd.AddCommand(exportRepoCmd)
 }
 
@@ -21,40 +20,35 @@ var exportRepoCmd = &cobra.Command{
 	Use:   "export-repo",
 	Short: "Export all published finding aids",
 	Run: func(cmd *cobra.Command, args []string) {
-		start := time.Now()
-		client, err := aspace.NewClient("fade", 20)
+		flag.Parse()
+		client, err := aspace.NewClient(env, 20)
 		HandleError(err)
 
-		err = exportRepo(repoID, client)
+		err = exportRepo(repositoryId, client)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("complete, run time:", time.Since(start))
 
 	},
 }
 
 func exportRepo(repoId int, client *aspace.ASClient) error {
-	repository, err := client.GetRepository(repoID)
+	repoString := fmt.Sprintf("%d", repositoryId)
+	out, err := os.Create(fmt.Sprintf("errors-repository-%s.txt", repoString))
 	if err != nil {
 		return err
 	}
-
-	err = os.Mkdir("exports", 0777)
-	if err != nil {
-		return err
-	}
-
-	logFile, err := os.Create(repository.Slug + ".txt")
-	if err != nil {
-		return err
-	}
-	defer logFile.Close()
-	writer := bufio.NewWriter(logFile)
+	defer out.Close()
+	writer := bufio.NewWriter(out)
 
 	resourceIds, err := client.GetResourceIDs(repoId)
 	if err != nil {
 		return fmt.Errorf("Could not get resource List for repository %d", repoId)
+	}
+
+	err = os.Mkdir(repoString, 0775)
+	if err != nil {
+		return fmt.Errorf("Could not create an export directory for repository %d", repoId)
 	}
 
 	for _, resourceId := range resourceIds {
@@ -64,15 +58,11 @@ func exportRepo(repoId int, client *aspace.ASClient) error {
 		}
 
 		if resource.Publish == true {
-			log.Println("attempting ", resource.EADID, resource.URI)
-			err = getEADFile(repoId, resourceId, "exports", resource.EADID, client)
+			log.Println("attempting", resource.EADID, resource.URI)
+			err = getEADFile(repoId, resourceId, repoString, resource.EADID, client)
 			if err != nil {
-				log.Println(err)
-				writer.WriteString(fmt.Sprintf("%s\t%s\t%v\n", resource.URI, "failure", err))
-				writer.Flush()
-			} else {
-				log.Println("success")
-				writer.WriteString(fmt.Sprintf("%s\t%s\n", resource.URI, "success"))
+				fmt.Println(err)
+				writer.WriteString(fmt.Sprintf("%s\t%v\n", resource.URI, err))
 				writer.Flush()
 			}
 		}
