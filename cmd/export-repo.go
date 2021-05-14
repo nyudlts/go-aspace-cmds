9d33d5dbc6534a9659cee7df62239f5976f35622
Don Mennerich <don.mennerich@nyu.edu>
2021-05-14 10:54:02 -0400

    updating export repo logic

--- cmd/export-repo.go
+++ cmd/export-repo.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func init() {
@@ -18,24 +19,28 @@ func init() {
 	rootCmd.AddCommand(exportRepoCmd)
 }
 
-var exportRepoCmd = &cobra.Command{
+var exportRepoCmd = &cobra.Command {
 	Use:   "export-repo",
 	Short: "Export all published finding aids",
 	Run: func(cmd *cobra.Command, args []string) {
 		flag.Parse()
-		client, err := aspace.NewClient(env, 20)
+		var err error;
+		client, err = aspace.NewClient(env, 20)
 		HandleError(err)
 
-		err = exportRepo(repositoryId, client)
+		fmt.Printf("go-aspace version %s\n", aspace.LibraryVersion)
+
+		err = exportRepo(repositoryId)
 		if err != nil {
 			fmt.Println(err)
+			os.Exit(1)
 		}
 
 	},
 }
 
-func exportRepo(repoId int, client *aspace.ASClient) error {
-	//repoString := fmt.Sprintf("%d", repositoryId)
+func exportRepo(repoId int) error {
+
 	out, err := os.Create(fmt.Sprintf("errors-repository-%s.txt", location))
 	if err != nil {
 		return err
@@ -43,25 +48,41 @@ func exportRepo(repoId int, client *aspace.ASClient) error {
 	defer out.Close()
 	writer := bufio.NewWriter(out)
 
-	resourceIds, err := client.GetResourceIDs(repoId)
+	repository, err := client.GetRepository(repositoryId);
 	if err != nil {
-		return fmt.Errorf("Could not get resource List for repository %d", repoId)
+		return fmt.Errorf("Repistory ID %d does not exist", repoId)
 	}
 
-	err = os.Mkdir(location, 0775)
+	slug := repository.Slug
+
+	outputDir := filepath.Join(location, slug)
+
+	err = os.MkdirAll(outputDir, 0775)
 	if err != nil {
 		return fmt.Errorf("Could not create an export directory for repository %s", repoId)
 	}
 
+	resourceIds, err := client.GetResourceIDs(repoId)
+		if err != nil {
+			return fmt.Errorf("Could not get resource List for repository %d", repoId)
+		}
+
+		if len(resourceIds) <= 0 {
+			return fmt.Errorf("Repository '%s' does not contain any resources to export", slug)
+		}
+
+
 	for _, resourceId := range resourceIds {
+
 		resource, err := client.GetResource(repoId, resourceId)
 		if err != nil {
 			fmt.Printf(fmt.Sprintf("Could not get resource %d from repo %d, skipping", resourceId, repoId))
 		}
 
+
 		if resource.Publish == true {
 			log.Println("attempting", resource.EADID, resource.URI)
-			err = getEADFile(repoId, resourceId, location, resource.EADID, pretty, client)
+			err = getEADFile(repoId, resourceId, outputDir, resource.EADID, pretty, client)
 			if err != nil {
 				fmt.Println(err)
 				writer.WriteString(fmt.Sprintf("%s\t%v\n", resource.URI, err))
